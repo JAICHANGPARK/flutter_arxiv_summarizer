baed29b2488a74590eaf3831c25c45e4fd6ead33
Dreamwalker <aristojeff@gmail.com>
2025-06-08 16:11:34 +0900

    Update arxiv_home_screen.g.dart

--- lib/ui/arxiv_home_screen.g.dart
+++ lib/ui/arxiv_home_screen.g.dart
@@ -6,7 +6,7 @@ part of 'arxiv_home_screen.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$geminiApiKeyStateHash() => r'4ffdc5e0276376a7da6d5c1c2d588fe4d0b365bf';
+String _$geminiApiKeyStateHash() => r'74bd4b04750746cac97ae3396f4bdf815bfc982e';
 
 /// See also [GeminiApiKeyState].
 @ProviderFor(GeminiApiKeyState)
@@ -22,5 +22,21 @@ final geminiApiKeyStateProvider =
     );
 
 typedef _$GeminiApiKeyState = Notifier<String>;
+String _$summarizedResultHash() => r'fd9042022d3959821711e6d28f2a6a46ad74b1f8';
+
+/// See also [SummarizedResult].
+@ProviderFor(SummarizedResult)
+final summarizedResultProvider =
+    AutoDisposeAsyncNotifierProvider<SummarizedResult, String>.internal(
+      SummarizedResult.new,
+      name: r'summarizedResultProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$summarizedResultHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SummarizedResult = AutoDisposeAsyncNotifier<String>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
