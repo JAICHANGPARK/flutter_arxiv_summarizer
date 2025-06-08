765a1fef2995fef13f471cfa27bdf62f9760d508
Dreamwalker <aristojeff@gmail.com>
2025-06-08 13:32:42 +0900

    Create arxiv_home_screen.dart

---
+++ lib/ui/arxiv_home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class ArxivHomeScreen extends StatefulWidget {
+  const ArxivHomeScreen({super.key});
+
+  @override
+  State<ArxivHomeScreen> createState() => _ArxivHomeScreenState();
+}
+
+class _ArxivHomeScreenState extends State<ArxivHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
