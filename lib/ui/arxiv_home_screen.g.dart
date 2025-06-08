6e93aebdd662c861caaf0b6a85152b6338e381dc
Dreamwalker <aristojeff@gmail.com>
2025-06-08 15:01:31 +0900

    Create arxiv_home_screen.g.dart

---
+++ lib/ui/arxiv_home_screen.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'arxiv_home_screen.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$geminiApiKeyStateHash() => r'4ffdc5e0276376a7da6d5c1c2d588fe4d0b365bf';
+
+/// See also [GeminiApiKeyState].
+@ProviderFor(GeminiApiKeyState)
+final geminiApiKeyStateProvider =
+    NotifierProvider<GeminiApiKeyState, String>.internal(
+      GeminiApiKeyState.new,
+      name: r'geminiApiKeyStateProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$geminiApiKeyStateHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$GeminiApiKeyState = Notifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
